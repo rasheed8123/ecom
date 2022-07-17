ac05016eda906ec0fbdce72edd7eb8ecfe09059e
Rasheed Ahmed <64723816+rasheed8123@users.noreply.github.com>
2022-07-17 14:39:37 +0530

    Update Brands.model.js

--- server/src/models/Brands.model.js
+++ server/src/models/Brands.model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const BrandSchema = new mongoose.Schema({
     Brand_Name:{type:String,required:true},
-    product_id : [{type : mongoose.Schema.Types.ObjectId, ref:"product", required : true}]
+    product_id : [{type : mongoose.Schema.Types.ObjectId, ref:"product", required : flase}]
 },{
     timestamps : true,
     versionKey : false,
